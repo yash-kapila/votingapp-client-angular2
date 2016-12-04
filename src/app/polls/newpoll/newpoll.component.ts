397cb032a085858c90c94b8d290e44ce707b76a7
Yash Kapila <yash.kapila18@gmail.com>
2016-12-04 17:47:24 +1100

    Add files via upload

---
+++ src/app/polls/newpoll/newpoll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newpoll',
+  templateUrl: './newpoll.component.html',
+  styleUrls: ['./newpoll.component.css']
+})
+export class NewpollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
