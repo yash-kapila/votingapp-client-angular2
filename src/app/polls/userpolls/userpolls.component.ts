2501a28a617e4fbc92fee882b03ff2f6134df160
Yash Kapila <yash.kapila18@gmail.com>
2016-12-04 17:49:01 +1100

    Add files via upload

---
+++ src/app/polls/userpolls/userpolls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userpolls',
+  templateUrl: './userpolls.component.html',
+  styleUrls: ['./userpolls.component.css']
+})
+export class UserpollsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
