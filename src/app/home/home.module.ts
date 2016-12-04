7a2adaaeb7f5390bba976ab1bb925cbb1857a443
Yash Kapila <yash.kapila18@gmail.com>
2016-12-04 17:39:39 +1100

    Add files via upload

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { PollsModule } from '../polls/polls.module';
+
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeService } from './home.service';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+      FormsModule,
+      PollsModule,
+      RouterModule.forRoot([
+          { path: 'home', component: HomeComponent },
+          { path: 'home/login', component: LoginComponent },
+          { path: 'home/signup', component: SignupComponent }
+      ])
+  ],
+  providers: [ 
+    HomeService  
+  ]
+})
+export class HomeModule { }
