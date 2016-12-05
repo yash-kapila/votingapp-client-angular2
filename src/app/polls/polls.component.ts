df681cb39c120ece545618d3df89dfa370b04170
Yash Kapila <yash.kapila18@gmail.com>
2016-12-05 14:12:40 +1100

    Update polls.component.ts

--- src/app/polls/polls.component.ts
+++ src/app/polls/polls.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { SharedService } from '../shared/shared.service';
 
 @Component({
   templateUrl: './polls.component.html',
@@ -7,14 +9,24 @@ import { Router } from '@angular/router';
 })
 export class PollsComponent implements OnInit {
   newPollView: boolean = true;
-  
-  constructor(private _router: Router) { }
+  private subscribe: any;
+
+  constructor(private _router: Router,
+              private _sharedService: SharedService) { }
 
   ngOnInit() {
+    // subscribe to SharedService property to listen for changes
+    this.subscribe = this._sharedService.getUserDetails().subscribe(
+      (username) => {
+        console.log("Listening to shared service from poll.component: " + username);
+      }
+    );
   }
 
   doLogout(): void{
     this._router.navigate(['/home']);
   }
+
+  ngOnDestroy() { }
 
 }
