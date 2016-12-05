d40e875069d588b50dc1ddb61d123be57073fd94
Yash Kapila <yash.kapila18@gmail.com>
2016-12-05 14:15:05 +1100

    Update newpoll.component.ts

--- src/app/polls/newpoll/newpoll.component.ts
+++ src/app/polls/newpoll/newpoll.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IPollOptions } from './poll-options';
+
 @Component({
   selector: 'app-newpoll',
   templateUrl: './newpoll.component.html',
   styleUrls: ['./newpoll.component.css']
 })
 export class NewpollComponent implements OnInit {
+  options: Array<IPollOptions> = [];
 
-  constructor() { }
+  constructor() {
+    this.options = [
+      {
+        'id': 1,
+        'value': ''
+      }
+    ] 
+  }
 
   ngOnInit() {
+  }
+
+  addMoreOptions(): void{
+    let lastOption = this.options[this.options.length - 1];
+    
+    this.options.push({
+      id: ++lastOption.id,
+      value: ''
+    })
+  }
+
+  submitPoll(): void{
+    console.log(this.options);
+
+    //send poll details to server
   }
 
 }
