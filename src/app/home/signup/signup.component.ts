1428520fea9d19e6fd2753e563a1c23c8791a8b0
Yash Kapila <yash.kapila18@gmail.com>
2016-12-05 14:10:32 +1100

    Update signup.component.ts

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { HomeService } from '../home.service';
+import { SharedService } from '../../shared/shared.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -11,7 +14,9 @@ export class SignupComponent implements OnInit {
   signup_password: string;
   signup_email: string;
   
-  constructor(private _router: Router) { 
+  constructor(private _router: Router, 
+              private _homeService: HomeService,
+              private _sharedService: SharedService) { 
     this.signup_email = "";
     this.signup_password = "";
     this.signup_username = "";
@@ -25,12 +30,17 @@ export class SignupComponent implements OnInit {
   }
 
   doSignup(): void{
-    console.log("Username: " + this.signup_username);
-    console.log("Email: " + this.signup_email);
-    console.log("Password: " + this.signup_password);
+    this._homeService.signUp().subscribe(
+      data => {
+        // set username for post login components use
+        this._sharedService.setUserDetails(data['username']);
 
-    // go to dashboard after successful signup
-    this._router.navigate(['/polls', 'dashboard']);
+        // go to dashboard after successful signup
+        this._router.navigate(['/polls', 'dashboard']);
+      },
+      error => {
+        console.log("Error in signup API request: " + error);
+      }
+    )
   }
-
 }
