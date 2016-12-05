2f5e09bba5cc27e36f483bf7c12c7ab4f665d342
Yash Kapila <yash.kapila18@gmail.com>
2016-12-05 14:13:50 +1100

    Update userpolls.component.ts

--- src/app/polls/userpolls/userpolls.component.ts
+++ src/app/polls/userpolls/userpolls.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { PollsService } from '../polls.service';
 
 @Component({
   selector: 'app-userpolls',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userpolls.component.css']
 })
 export class UserpollsComponent implements OnInit {
+  polls: Array<any[]>;
 
-  constructor() { }
+  constructor(private _pollsService: PollsService) { 
+    this.polls = [];
+  }
 
   ngOnInit() {
+    // retrieve list of polls for the user
+    this._pollsService.retrievePolls().subscribe(
+      data => {
+        this.polls = data['polls'];
+      },
+      error => {
+        console.log("Error in retrievePolls API request " + error);
+        this.polls = [];
+      }
+    );   
   }
+
+  deletePoll(poll): void{
+    console.log(JSON.stringify(poll));
+  }
+
+  ngOnDestroy() { }
 
 }
