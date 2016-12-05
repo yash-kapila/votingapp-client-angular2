a781f7cc2364d1431dfd33acf1c30702bb03eba2
Yash Kapila <yash.kapila18@gmail.com>
2016-12-05 14:08:56 +1100

    Update home.service.ts

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class HomeService {
+  loginURL = "api/home/login.json";
+  signupURL = "api/home/signup.json";
 
   constructor(private _http: Http) { }
 
   login(): Observable<Response> {
-    console.log("inside home service");
-    return this._http.get('/api/getpolldetails');
+    return this._http.get(this.loginURL)
+              .map((resp: Response) => resp.json());
+  }
+
+  signUp(): Observable<Response> {
+    return this._http.get(this.signupURL)
+              .map((resp: Response) => resp.json());
   }
 }
